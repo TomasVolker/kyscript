35cfb4a74dbaa4f928ece40ca7291f72549a9cec
tomasvolker <tomasvolker@gmail.com>
2019-03-26 20:39:01 +0100

    Update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.3.0"
+    kotlin("jvm") version "1.3.21"
+    maven
 }
 
 group = "tomasvolker"
-version = "0.0.1"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -14,18 +13,4 @@ repositories {
 dependencies {
     compile(kotlin("stdlib"))
     testCompile("junit", "junit", "4.12")
-}
-/*
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-*/
-val sourcesJar = tasks.create<Jar>("sourcesJar") {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    classifier = "sources"
-    from(sourceSets["main"].allSource)
 }
